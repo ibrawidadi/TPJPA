ec9ac7bc36482a70e51add1173739195787ac503
IBRAHIM <ibrawidadieuh2003@gmail.com>
2024-10-02 12:01:03 +0100

    YGY

---
+++ src/main/java/com/example/tpjpa/TpjpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpjpaApplication.class, args);
+    }
+
+}
